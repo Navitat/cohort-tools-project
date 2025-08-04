3bf61b87c9eee7d900ec542f3520db3296c7e7e6
navitat <anlon636@gmail.com>
2025-08-04 14:45:39 +0200

    feat: create post user auth route

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+const router = express.Router();
+
+const User = require("../models/User.model");
+
+const saltRounds = 10;
+
+// POST /auth/signup
+router.post("/signup", (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  if (!email || !password || !name) {
+    res.status(400).json({ message: "Provide email, password and name" });
+    return;
+  }
+
+  // regex validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: "Provide a valid email address." });
+    return;
+  }
+
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({
+      message:
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+
+  // check if email exists on db
+  User.findOne({ email })
+    .then((foundUser) => {
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists." });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      const newUser = {
+        email: email,
+        password: hashedPassword,
+        name: name,
+      };
+
+      return User.create(newUser);
+    })
+    .then((createdUser) => {
+      // deconstruct to not show password
+      const { email, name, _id } = createdUser;
+
+      const user = { email, name, _id };
+
+      res.status(201).json({ user: user });
+    })
+    .catch((error) => {
+      console.log("Error trying to create an account.");
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
