07d9ef977179e4b86dec159673e08640820f0af7
navitat <anlon636@gmail.com>
2025-08-04 14:24:59 +0200

    feat: create user model

---
+++ server/models/User.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    Type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("User", UserSchema);
